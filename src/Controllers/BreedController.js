5d55cbf16bff2edd1eeb003a64e51a761614ef5d
MarioGeorgiev <37220117+MarioGeorgiev@users.noreply.github.com>
2023-02-11 17:41:54 +0200

    add error handling for breed

--- src/Controllers/BreedController.js
+++ src/Controllers/BreedController.js
@@ -1,11 +1,16 @@
 const Breed = require('../models/Breed')
-
+const errorUtil = require('../lib/errorUtil')
 exports.GetAddBreed = (req, res) => {
   res.render('addBreed')
 }
 
 exports.PostAddBreed =async (req, res) => {
-  const breed = new Breed({ name: req.body.breed });
-  await breed.save();
+  try{
+    const breed = new Breed({ name: req.body.breed });
+    await breed.save();
+
+  }catch(error){
+    return res.render('addBreed', { error: errorUtil.getErrorMessage(error)})
+  }
   res.redirect('/')
 }
--- src/models/Breed.js
+++ src/models/Breed.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const BreedSchema = mongoose.Schema({
     name :{
         type : String,
-        required : true
+        required : [true, "Breed is required"]
     }
 })
 
